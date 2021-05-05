50c07205b20b542e53b5d4b778c54d70d3f4d1db
asvesj <jonathan.asvestis@emc.com>
2021-05-05 15:30:26 +0800

    Delete setup.sh

--- dynamic-secrets-k8s/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-echo "##################################################"
-echo "Install shipyard if you do not have it installed"
-echo "curl https://shipyard.demo.gs/install.sh | bash"
-echo "##################################################"
-
-yard up --enable-consul false
