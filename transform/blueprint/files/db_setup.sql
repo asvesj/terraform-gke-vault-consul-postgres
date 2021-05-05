f371db60d77304e18553f79dd66a09580dca8cf1
asvesj <jonathan.asvestis@emc.com>
2021-05-05 15:29:51 +0800

    Delete transform directory

--- transform/blueprint/files/db_setup.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE orders (
-    id SERIAL PRIMARY KEY, 
-    card_number VARCHAR (255) NOT NULL,
-    created_at TIMESTAMP NOT NULL, 
-    updated_at TIMESTAMP NOT NULL, 
-    deleted_at TIMESTAMP
-);
